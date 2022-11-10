d020acc1b85c14ffcdfa409bd420bce4b65eb94f
RenilNavadiya <renilnavadiya3131@gmail.com>
2022-11-10 00:52:16 -0500

    initial commit

---
+++ UserManagement_Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UserManagement_Project.MvcApplication" Language="C#" %>
---
+++ UserManagement_Project/Models/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserManagement_Project.Models
+{
+    public class UserDTO
+    {
+    }
+}
