2eb020a7225a74315dc73107fe586873ac660184
Maitri Patel <maitribalar@gmail.com>
2022-11-10 22:32:09 -0500

    Connection string & Calling it in UserRepository completed

--- UMP_DataAccess/DataConnection.cs
+++ UMP_DataAccess/DataConnection.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UMP_DataAccess
 {
-    class DataConnection
+    public class DataConnection
     {
+        public DataConnection(SqlTransaction trans = null)
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["UserManagementDsn"].ToString();
+            _trans = trans;
+        }
+
+        private string _connectionString { get; set; }
+        public string ConnectionString => _connectionString;
+
+        private SqlTransaction _trans { get; set; }
+        public SqlTransaction Trans => _trans;
     }
 }
