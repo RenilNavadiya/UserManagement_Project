dfe2e4adb59c6a9b54c2bb49a19963f8865206e7
Maitri Patel <maitribalar@gmail.com>
2022-11-11 13:38:52 -0500

    Adding Properties in DTO and Entities and Mapper class completed

--- UMP_DataObject/UserEntity.cs
+++ UMP_DataObject/UserEntity.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace UMP_DataObject
 {
-    class UserEntity
+    public class UserEntity
     {
+        //Declaring Variables
+
+        private Guid _UserId;
+        private string _FirstName;
+        private string _LastName;
+        private DateTime _DateOfBirth;
+        private string _Gender;
+        private string _Street;
+        private string _City;
+        private string _Province;
+        private string _Country;
+        private string _PostalCode; 
+
+        //Setting Values
+
+        public Guid UserId { get { return _UserId; } set { _UserId= value; } }
+
+        public string FirstName { get { return _FirstName;} set { _FirstName= value; } }
+
+        public string LastName { get { return _LastName;} set { _LastName= value; } }
+
+        public DateTime DateOfBirth { get { return _DateOfBirth; } set { _DateOfBirth= value; } }
+
+        public string Gender { get { return _Gender;} set { _Gender= value; } } 
+
+        public string Street { get { return _Street;} set { _Street= value; } }
+
+        public string City { get { return _City;} set { _City = value; } }
+
+        public  string Province { get { return _Province;} set { _Province= value; } }  
+
+        public string Country { get { return _Country;} set { _Country= value; } }
+
+        public string PostalCode { get { return _PostalCode;} set { _PostalCode= value; } }
+
+
+
     }
 }
