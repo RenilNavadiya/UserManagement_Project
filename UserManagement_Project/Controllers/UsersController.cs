e1dabcac3f7d21423c799e908fc5f4d632b9738d
Parth Darji <parthdarji20@gmail.com>
2022-11-12 19:42:38 -0500

    get all users data functionality completed

--- UserManagement_Project/Controllers/UsersController.cs
+++ UserManagement_Project/Controllers/UsersController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UMP_DataAccess;
+using UserManagement_Project.Mapper;
+using UserManagement_Project.Models;
 
 namespace UserManagement_Project.Controllers
 {
@@ -11,7 +14,21 @@ namespace UserManagement_Project.Controllers
         // GET: Users
         public ActionResult Index()
         {
-            return View();
+            UserRepository userData = new UserRepository();
+            List<UserDTO> userDTOs = new List<UserDTO>();
+            try
+            {
+                var entityData = userData.GetUsers();
+                foreach(var item in entityData)
+                {
+                    userDTOs.Add(item?.Map());
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+            }
+            return View(userDTOs);
         }
     }
 }
